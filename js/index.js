46ab9b90093965c676df67132c969c9411c91316
lotuslwb <837531387@qq.com>
2015-04-27 16:54:29 +0800

    add gif for A

--- js/index.js
+++ js/index.js
@@ -14,16 +14,31 @@ $(function(){
         $('.arrowbox').click(function(){
           swiper.swipeNext();
         });
-            
+        
+        var loaded = 0;
         if ($('body img').length === 0) {
             show_page();
         } else {
             $('body img').imagesLoaded().progress(function(instance, image) {
+              loaded++;
+              if (loaded == instance.images.length) {
                 show_page();
+                setTimeout(showGif,1000)
+              }
             });
         }
         
         function show_page() {
             $(".page-loading").fadeOut();
         }
+
+    function showGif(){
+      var $fall=$('<img src="img/fall.gif" class="fall">');
+      $fall.appendTo($('body'));
+      setTimeout(function(){
+        $fall.remove();
+      },4000)
+    }
 })
+
+
